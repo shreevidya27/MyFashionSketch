c17f6b8214d43b425a8715ad988fb5540f0c9e95
shreevidya srinivasan <shreevirosy@gmail.com>
2022-03-31 20:25:33 +0530

    Added fragment

--- app/src/main/java/com/myapp/myfashionsketch/DetailedActivity.java
+++ app/src/main/java/com/myapp/myfashionsketch/DetailedActivity.java
@@ -1,24 +1,42 @@
 package com.myapp.myfashionsketch;
 
-import android.content.Intent;
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 
-public class DetailedActivity extends AppCompatActivity {
-
+public class DetailedActivity extends Activity {
+    private static final int CONTENT_VIEW_ID = 10101010;
     @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailed);
-        // Get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(HomeActivity.EXTRA_MESSAGE);
+        FrameLayout frame = new FrameLayout(this);
+        frame.setId(CONTENT_VIEW_ID);
+        setContentView(frame, new FrameLayout.LayoutParams(
+                FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT));
 
-        TextView textView = findViewById(R.id.detailedView);
-        textView.setText(message);
+        if (savedInstanceState == null) {
+            Fragment newFragment = new DebugExampleTwoFragment();
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(CONTENT_VIEW_ID,newFragment).commit();
+        }
+    }
+
+    public static class DebugExampleTwoFragment extends Fragment {
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            EditText v = new EditText(getActivity());
+            v.setText("Hello Fragment!");
+            return v;
+        }
     }
 }
--- app/src/main/java/com/myapp/myfashionsketch/HomeActivity.java
+++ app/src/main/java/com/myapp/myfashionsketch/HomeActivity.java
@@ -9,6 +9,8 @@ import android.widget.ImageView;
 public class HomeActivity extends AppCompatActivity {
     ImageView imageView1;
     ImageView imageView2;
+    ImageView imageview3;
+    ImageView imageView4;
     public static final String EXTRA_MESSAGE = "com.example.fashion.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +18,13 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         imageView1 = findViewById(R.id.imageView1);
         imageView2 = findViewById(R.id.imageView2);
+        imageview3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
 
         imageView1.setOnClickListener(this::onClick);
+        imageView2.setOnClickListener(this::onClick);
+        imageview3.setOnClickListener(this::onClick);
+        imageView4.setOnClickListener(this::onClick);
     }
 
     private void onClick(View view) {
