5c210a07ba47d7e843f30a1a19ae09b7d7538d98
shreevidya <shreevirosy@gmail.com>
2022-04-06 19:33:49 +0530

    fixed horzontal layout

--- app/src/main/java/com/myapp/myfashionsketch/DetailedActivity.java
+++ app/src/main/java/com/myapp/myfashionsketch/DetailedActivity.java
@@ -47,7 +47,7 @@ public class DetailedActivity extends Activity {
                                  Bundle savedInstanceState) {
             View rootView =inflater.inflate(R.layout.activity_detailed, container, false);
             recyclerView = rootView.findViewById(R.id.recycler);
-            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false));
             data.add("https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8aHVtYW58ZW58MHx8MHw%3D&ixlib=rb-1.2.1&w=1000&q=80");
             data.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg");
             data.add("https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxleHBsb3JlLWZlZWR8M3x8fGVufDB8fHw%3D&w=1000&q=80");
